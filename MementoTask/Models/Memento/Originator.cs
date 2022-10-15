562be4225ebcfabc96b9c38ddff4cfd7ae76ebc9
Ali Ahmadov <aahmadov7626@ada.edu.az>
2022-10-16 00:40:57 +0400

    all done

---
+++ MementoTask/Models/Memento/Originator.cs
@@ -0,0 +1,41 @@
+﻿using MementoTask.Models.ScreenshoteService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace MementoTask.Models.Memento
+{
+    public class Originator
+    {
+        private ImageSource _state;
+
+        public Originator(ImageSource state)
+        {
+            _state = state;
+        }
+
+        public void TakeScreenShot(ImageSource imageSource)
+        {
+            _state = imageSource;
+        }
+
+        public IMemento Save()
+        {
+            return new ConcreteMemento(this._state);
+        }
+
+        public void Restore(IMemento memento)
+        {
+            if (!(memento is ConcreteMemento))
+            {
+                return;
+            }
+
+            this._state = memento.GetImageSource();
+        }
+    }
+}
---
+++ MementoTask/ViewModels/AppViewModel.cs
@@ -0,0 +1,60 @@
+﻿using MementoTask.Commands;
+using MementoTask.Models;
+using MementoTask.Models.Memento;
+using MementoTask.Models.ScreenshoteService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace MementoTask.ViewModels
+{
+    public class AppViewModel : BaseViewModel
+    {
+        private ImageSource imageSource;
+
+        public ImageSource ImageSource
+        {
+            get { return imageSource; }
+            set { imageSource = value; OnPropertyChanged(); }
+        }
+
+
+        public RelayCommand NextCommand { get; set; }
+        public RelayCommand PrevCommand { get; set; }
+        public RelayCommand ScreenShotCommand { get; set; }
+
+
+        public AppViewModel()
+        {
+
+            Originator originator = new Originator(ImageSource);
+            CareTaker careTaker = new CareTaker(originator);
+            ScreenShotCommand = new RelayCommand(c =>
+            {
+                var result = ScreenCapture.CaptureActiveWindow();
+                var imageSource = BitmapConverter.ImageSourceFromBitmap(result);
+                ImageSource = imageSource;
+                originator.TakeScreenShot(ImageSource);
+                careTaker.Backup();
+            });
+
+            PrevCommand = new RelayCommand(c =>
+            {
+                var imageSource = careTaker.Undo();
+                if (imageSource != null)
+                    ImageSource = imageSource;
+            });
+
+
+            NextCommand = new RelayCommand(c =>
+            {
+                var imageSource = careTaker.Redo();
+                if (imageSource != null)
+                    ImageSource = imageSource;
+            });
+        }
+    }
+}
